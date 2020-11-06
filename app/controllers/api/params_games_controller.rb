96e7a1f7c65379d4913059e1050eded0fa1261f8
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-05 20:37:39 -0500

    Added first letter of first name = to a gives back a message

--- app/controllers/api/params_games_controller.rb
+++ app/controllers/api/params_games_controller.rb
@@ -1,6 +1,9 @@
 class Api::ParamsGamesController < ApplicationController
   def query
-    @name = params["name"].upcase
+    @name = params["name"]
+    if @name[0] == "a"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    end
     render 'query.json.jb'
   end
 end
